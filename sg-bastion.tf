2f5942b9a651a681154f580faf424ddae2e3c709
maximm <maxim.mandelblum@imperva.com>
2021-03-16 20:49:50 +0200

    updated_project

---
+++ sg-bastion.tf
@@ -0,0 +1,20 @@
+# resource "aws_security_group" "bastion" {
+#     name = "bastion"
+#     description = "bastion to connect private servers"
+#     vpc_id = module.vpc.vpc_id
+
+#     ingress  {
+#         from_port = 22
+#         to_port = 22
+#         protocol = "tcp"
+#         cidr_blocks = ["80.179.69.192/26"]
+#     }
+    
+#     egress  {
+#         from_port = 22
+#         to_port = 22
+#         protocol = "tcp"
+#         cidr_blocks = ["0.0.0.0/0"]
+#     }
+
+# }
